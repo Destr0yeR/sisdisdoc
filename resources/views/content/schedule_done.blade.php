f681fe67ba91a2cbe622ce77472e331394e226ee
fama1994@gmail.com <fama1994@gmail.com>
2016-02-22 19:12:30 -0500

    styles en done

--- resources/views/content/schedule_done.blade.php
+++ resources/views/content/schedule_done.blade.php
@@ -1,29 +1,43 @@
-<div class="row">
-	<div class="col-sm-10 col-sm-offset-1">
-		<table class="table table-bordered">
-			<tr>
-				<td></td>
-				<td>Lunes</td>
-				<td>Martes</td>
-				<td>Miércoles</td>
-				<td>Jueves</td>
-				<td>Viernes</td>
-				<td>Sábado</td>
-				<td>Domingo</td>
-			</tr>
-			@for($i = 1 ; $i <= 14 ; ++$i)
-				<tr>
-					<td>{{ $i + 7 }} - {{ $i + 8 }}</td>
-
-					@for($j = 1 ; $j <= 7 ; ++$j)
-						<td class="schedule-button">
-							<a class="schedule-link @if($schedule[ 7*($i-1) + $j ]) selected @endif" id="cell{{ 7*($i-1) + $j }}">
-								
-							</a>
-						</td>
-					@endfor
-				</tr>
-			@endfor
-		</table>
+<div class="row" >
+	<div class="col-sm-10 col-sm-offset-1 section-header">	
+			1-Disponibilidad De Horarios
 	</div>
 </div>
+
+<div>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1 schedule-header">
+			Seleccione la casilla correspondiente a la hora del dia en la que quiere dictar un curso
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1">
+			<table class="table table-bordered">
+				<tr>
+					<td class="schedule-static-header"></td>
+					<td class="schedule-static-header">Lunes</td>
+					<td class="schedule-static-header">Martes</td>
+					<td class="schedule-static-header">Miércoles</td>
+					<td class="schedule-static-header">Jueves</td>
+					<td class="schedule-static-header">Viernes</td>
+					<td class="schedule-static-header">Sábado</td>
+					<td class="schedule-static-header">Domingo</td>
+				</tr>
+				@for($i = 1 ; $i <= 14 ; ++$i)
+					<tr>
+						<td class="schedule-static-header">{{ $i + 7 }} - {{ $i + 8 }}</td>
+
+						@for($j = 1 ; $j <= 7 ; ++$j)
+							<td class="schedule-button">
+								<a class="schedule-link @if($schedule[ 7*($i-1) + $j ]) selected @endif" id="cell{{ 7*($i-1) + $j }}">
+									
+								</a>
+							</td>
+						@endfor
+					</tr>
+				@endfor
+			</table>
+		</div>
+	</div>
+</div>
+
--- resources/views/content/subjects_done.blade.php
+++ resources/views/content/subjects_done.blade.php
@@ -1,9 +1,14 @@
+<div class="row" >
+  <div class="col-sm-10 col-sm-offset-1 section-header">  
+      2-Preferencias de Cursos
+  </div>
+</div>
 <div class="row">
 	<div class="col-sm-10 col-sm-offset-1 subject subject-header">
 		<h2>Mis Cursos</h2>
 	</div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom:30px;">
 	<div class="col-sm-10 col-sm-offset-1  subject subject-content">
 		<div class="row" id="subject-items">
       @foreach($subjects as $subject)
